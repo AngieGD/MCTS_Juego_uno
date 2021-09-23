cbe748936281b391c4b41cc3d8d990287e560713
Camilo <juancmaringcu@gmail.com>
2021-09-23 16:28:46 -0500

    Cambio a tic tac toe

---
+++ Uno/MCTS.py
@@ -0,0 +1,12 @@
+class MCTS:
+    def seleccion(self):
+        pass
+    def expancion(self):
+        pass
+    def simulacion(self):
+        pass
+
+    def backpropagation(self):
+        pass
+
+    
