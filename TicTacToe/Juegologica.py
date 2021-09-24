6da3512abe973e2a4c1393e5412cc0564be18a20
edwardrain27 <pablo522648@gmail.com>
2021-09-23 20:18:42 -0500

    imprimir tablero

--- TicTacToe/Juegologica.py
+++ TicTacToe/Juegologica.py
@@ -2,47 +2,13 @@
 import numpy as np
 import pandas as pd 
 
-""" 
-
-#creación del tablero vacío
-table = np.array([ [[ ['','',''],
-                     ['','',''],
-                     ['','',''] ],
-                     [ ['','',''],
-                     ['','',''],
-                     ['','',''] ],
-                     [ ['','',''],
-                     ['','',''],
-                     ['','',''] ] ],
-                     [[ ['','',''],
-                     ['','',''],
-                     ['','',''] ],
-                     [ ['','',''],
-                     ['','',''],
-                     ['','',''] ],
-                     [ ['','',''],
-                     ['','',''],
-                     ['','',''] ] ],
-                     [[ ['','',''],
-                     ['','',''],
-                     ['','',''] ],
-                     [ ['','',''],
-                     ['','',''],
-                     ['','',''] ],
-                     [ ['','',''],
-                     ['','',''],
-                     ['','',''] ] ]
-                ])
-
-
-print(table.shape) """
 
 class Table:
 
     def __init__(self):
-        self.table = [[1,2,3],
-                      [4,5,6],
-                      [7,8,9]]
+        self.table = [['','',''],
+                      ['','',''],
+                      ['','','']]
         
         
 
@@ -58,50 +24,38 @@ class Table:
         return moves
 
     
+def crearTablero():
+    tablero_grande = np.zeros((3,3)).astype('object')
+
+    for row in range(tablero_grande.shape[0]):
+        for col in range(tablero_grande.shape[1]):
+            tablero_grande[row,col] = Table()
+    return tablero_grande
 
 
+tablero = crearTablero()
 
-
-tablero = Table()
-
-
-tablero_grande = np.zeros((3,3)).astype('object')
-
-for row in range(tablero_grande.shape[0]):
-    for col in range(tablero_grande.shape[1]):
-        tablero_grande[row,col] = Table()
-
-
-def imprimirTablero(superTablero):
-
+def imprimirTablero(tablero):
     salida = ""
     matriz = ""
-    for fila in superTablero:
-        for table in fila:
-            #primera matriz
-            for row in table.table:
-                for col in row:
-                    salida+=str(col) +'|' 
-                salida+="\n" #1 fila 
-            matriz += salida
-            #matriz = salida
-            salida = ""    
-        matriz+="\n" 
+    for fila in tablero:
+        for i in range(3):
+            for table in fila:
+                salida += repr(table.table[i])+" "
+            matriz+=salida+"\n"
+            salida = ""
+        matriz+="\n"
     print(matriz)
+
+imprimirTablero(tablero)
+
+ganar = False
+
+
     
 
 
-imprimirTablero(tablero_grande)
 
-""" 
-jugadores = [{'nombre':'Eduardo', 'escogido':''}, {'nombre':'Bot', 'escogido':''}]
-
-for jugador in jugadores:
-    print(jugador)
-
-
-
- """
 
 
  
